73f788d818c794fd3d138d3f688124187764ab90
daniabil <ahmaddanii092@gmail.com>
2024-01-31 16:42:01 +0700

    feature : Image Bedroom

--- datas/dataKamar.js
+++ datas/dataKamar.js
@@ -79,70 +79,70 @@ suite = [
         "rating": 4.7,
         "harga": 3500000,
         "kode_kamar": "SUITE001",
-        "gambar": 1
+        "gambar": "https://images.pexels.com/photos/262048/pexels-photo-262048.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Luxury Suite",
         "rating": 4.5,
         "harga": 3000000,
         "kode_kamar": "SUITE002",
-        "gambar": 2
+        "gambar": "https://images.pexels.com/photos/1454806/pexels-photo-1454806.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Presidential Suite",
         "rating": 4.4,
         "harga": 2900000,
         "kode_kamar": "SUITE003",
-        "gambar": 3
+        "gambar": "https://images.pexels.com/photos/237371/pexels-photo-237371.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Honeymoon Suite",
         "rating": 4.5,
         "harga": 3100000,
         "kode_kamar": "SUITE004",
-        "gambar": 4
+        "gambar": "https://images.pexels.com/photos/2631746/pexels-photo-2631746.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Royal Suite",
         "rating": 4.6,
         "harga": 3300000,
         "kode_kamar": "SUITE005",
-        "gambar": 5
+        "gambar": "https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Deluxe Suite",
         "rating": 4.4,
         "harga": 2900000,
         "kode_kamar": "SUITE006",
-        "gambar": 6
+        "gambar": "https://images.pexels.com/photos/279746/pexels-photo-279746.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Grand Suite",
         "rating": 4.5,
         "harga": 3000000,
         "kode_kamar": "SUITE007",
-        "gambar": 7
+        "gambar": "https://images.pexels.com/photos/1743229/pexels-photo-1743229.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Family Suite",
         "rating": 4.4,
         "harga": 2800000,
         "kode_kamar": "SUITE008",
-        "gambar": 8
+        "gambar": "https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Luxury Oceanfront Suite",
         "rating": 4.6,
         "harga": 3800000,
         "kode_kamar": "SUITE009",
-        "gambar": 9
+        "gambar": "https://images.pexels.com/photos/2416932/pexels-photo-2416932.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Panoramic View Suite",
         "rating": 4.6,
         "harga": 3000000,
         "kode_kamar": "SUITE010",
-        "gambar": 10
+        "gambar": "https://images.pexels.com/photos/2029731/pexels-photo-2029731.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     }
 ]
 
@@ -155,70 +155,70 @@ superior = [
         "rating": 4.9,
         "harga": 5000000,
         "kode_kamar": "SPR001",
-        "gambar_kamar": 1
+        "gambar_kamar": "https://images.pexels.com/photos/7045767/pexels-photo-7045767.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Executive Superior Suite",
         "rating": 5.0,
         "harga": 6000000,
         "kode_kamar": "SPR002",
-        "gambar_kamar": 2
+        "gambar_kamar": "https://images.pexels.com/photos/6186810/pexels-photo-6186810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Premier Superior Room",
         "rating": 4.8,
         "harga": 4950000,
         "kode_kamar": "SPR003",
-        "gambar_kamar": 3
+        "gambar_kamar": "https://images.pexels.com/photos/6585619/pexels-photo-6585619.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Grand Superior Chamber",
         "rating": 5.0,
         "harga": 5900000,
         "kode_kamar": "SPR004",
-        "gambar_kamar": 4
+        "gambar_kamar": "https://images.pexels.com/photos/7214471/pexels-photo-7214471.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Luxury Superior Retreat",
         "rating": 4.9,
         "harga": 5650000,
         "kode_kamar": "SPR005",
-        "gambar_kamar": 5
+        "gambar_kamar": "https://images.pexels.com/photos/3144580/pexels-photo-3144580.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Superior King Oasis",
         "rating": 4.8,
         "harga": 5050000,
         "kode_kamar": "SPR006",
-        "gambar_kamar": 6
+        "gambar_kamar": "https://images.pexels.com/photos/2030119/pexels-photo-2030119.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Elite Superior Accommodation",
         "rating": 5.0,
         "harga": 6300000,
         "kode_kamar": "SPR007",
-        "gambar_kamar": 7
+        "gambar_kamar": "https://images.pexels.com/photos/2506990/pexels-photo-2506990.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Signature Superior Residency",
         "rating": 4.9,
         "harga": 5450000,
         "kode_kamar": "SPR008",
-        "gambar_kamar": 8
+        "gambar_kamar": "https://images.pexels.com/photos/6587907/pexels-photo-6587907.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Platinum Superior Haven",
         "rating": 4.8,
         "harga": 5000000,
         "kode_kamar": "SPR009",
-        "gambar_kamar": 9
+        "gambar_kamar": "https://images.pexels.com/photos/6758770/pexels-photo-6758770.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Exclusive Superior Retreat",
         "rating": 5.0,
         "harga": 6400000,
         "kode_kamar": "SPR010",
-        "gambar_kamar": 10
+        "gambar_kamar": "https://images.pexels.com/photos/5461604/pexels-photo-5461604.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     }
 ]
 
@@ -230,70 +230,70 @@ standar = [
         "rating": 3.2,
         "harga": 2000000,
         "kode_kamar": "STD001",
-        "gambar_kamar": 1
+        "gambar_kamar": "https://images.pexels.com/photos/1457845/pexels-photo-1457845.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Classic Standard Room",
         "rating": 3.2,
         "harga": 950000,
         "kode_kamar": "STD002",
-        "gambar_kamar": 2
+        "gambar_kamar": "https://images.pexels.com/photos/1329711/pexels-photo-1329711.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Standard Twin Room",
         "rating": 3.4,
         "harga": 2980000,
         "kode_kamar": "STD003",
-        "gambar_kamar": 3
+        "gambar_kamar": "https://images.pexels.com/photos/3209035/pexels-photo-3209035.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Traditional Standard Chamber",
         "rating": 3.3,
         "harga": 2050000,
         "kode_kamar": "STD004",
-        "gambar_kamar": 4
+        "gambar_kamar": "https://images.pexels.com/photos/271743/pexels-photo-271743.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Basic Standard Accommodation",
         "rating": 3.4,
         "harga": 2970000,
         "kode_kamar": "STD005",
-        "gambar_kamar": 5
+        "gambar_kamar": "https://images.pexels.com/photos/1571450/pexels-photo-1571450.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Standard Queen Room",
         "rating": 3.2,
         "harga": 1000000,
         "kode_kamar": "STD006",
-        "gambar_kamar": 6
+        "gambar_kamar": "https://images.pexels.com/photos/1743231/pexels-photo-1743231.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Cozy Standard Retreat",
         "rating": 3.3,
         "harga": 1930000,
         "kode_kamar": "STD007",
-        "gambar_kamar": 7
+        "gambar_kamar": "https://images.pexels.com/photos/210604/pexels-photo-210604.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Standard Single Room",
         "rating": 3.2,
         "harga": 980000,
         "kode_kamar": "STD008",
-        "gambar_kamar": 8
+        "gambar_kamar": "https://images.pexels.com/photos/2062431/pexels-photo-2062431.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Comfort Standard Suite",
         "rating": 3.2,
         "harga": 950000,
         "kode_kamar": "STD009",
-        "gambar_kamar": 9
+        "gambar_kamar": "https://images.pexels.com/photos/53603/bedroom-architectural-interior-lifestyle-53603.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         "nama_kamar": "Standard Economy Chamber",
         "rating": 3.4,
         "harga": 1750000,
         "kode_kamar": "STD010",
-        "gambar_kamar": 10
+        "gambar_kamar": "https://images.pexels.com/photos/1543439/pexels-photo-1543439.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     }
 ]
 
